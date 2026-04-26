cb4760675721ba3ffeef864c1147750cd9c39bfa
w4nder1ng06 <kquoc0449@gmail.com>
2026-04-26 22:54:17 +0700

    BETA - Multiple scales support
    The tool now can handle different scales problem (TESTED ON 1080P WITH SCALES: 100%, 125%, 150%, 175%).

--- core_functions/vision.py
+++ core_functions/vision.py
@@ -1,41 +1,44 @@
 import cv2
 import numpy as np
 import pyautogui
-import pygetwindow as gw
+import ctypes
+try:
+    ctypes.windll.shcore.SetProcessDpiAwareness(2)
+except:
+    ctypes.windll.user32.SetProcessDPIAware()
 
-def find_coord(target_path, threshold = 0.7, window_name = None):
+
+def get_true_scale():
+    width_phys = ctypes.windll.user32.GetSystemMetrics(0)
+    width_logic, _ = pyautogui.size()
+    return width_phys / width_logic
+
+
+def find_coord(target_path, threshold=0.7, window_name=None):
+    scale_factor = get_true_scale()
     template = cv2.imread(target_path)
-    if template is None:
-        return None
-    offset_x, offset_y = 0, 0
-    region = None
-    if window_name:
-        try:
-            win = gw.getWindowsWithTitle(window_name)[0]
-            # Define the region: (left, top, width, height)
-            region = (win.left, win.top, win.width, win.height)
-            offset_x, offset_y = win.left, win.top
-        except:
-            return None
-    scales = np.linspace(0.4, 1.6, 30)
-    screenshot = np.array(pyautogui.screenshot(region=region))
+
+    if template is None: return None
+
+    screenshot = np.array(pyautogui.screenshot())
     frame = cv2.cvtColor(screenshot, cv2.COLOR_RGB2BGR)
+    scales = np.linspace(0.8, 2.2, 20)
     best_match = None
     max_score = -1
-    for scale in scales:
-        width = int(template.shape[1] * scale)
-        height = int(template.shape[0] * scale)
-        resized_template = cv2.resize(template, (width, height))
-        if height > frame.shape[0] or width > frame.shape[1]:
-            continue
-        result = cv2.matchTemplate(frame, resized_template, cv2.TM_CCOEFF_NORMED)
-        _, max_val, _, max_loc = cv2.minMaxLoc(result)
-        if max_score < max_val:
+
+    for s in scales:
+        w = int(template.shape[1] * s)
+        h = int(template.shape[0] * s)
+        if h > frame.shape[0] or w > frame.shape[1]: continue
+        res = cv2.matchTemplate(frame, cv2.resize(template, (w, h)), cv2.TM_CCOEFF_NORMED)
+        _, max_val, _, max_loc = cv2.minMaxLoc(res)
+        if max_val > max_score:
             max_score = max_val
-            best_match = (max_loc[0] + width//2 + offset_x, max_loc[1] + height//2 + offset_y, max_val)
-            if max_val >= 0.9:
-                return best_match
+            phys_x = max_loc[0] + w // 2
+            phys_y = max_loc[1] + h // 2
+            best_match = (phys_x / scale_factor, phys_y / scale_factor, max_val)
+            if max_val >= 0.9: break
+
     if max_score >= threshold:
         return best_match
     return None
-
